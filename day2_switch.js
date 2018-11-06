cb7f0a1bcd83d98fd5b8485dfb32e07e8216bbfa
coooopeeeer <miminashicooper@gmail.com>
2018-11-06 13:33:38 -0800

    Fix case conditional

--- day2_switch.js
+++ day2_switch.js
@@ -1,21 +1,17 @@
 /*
-    Day2: Conditional Statements: If-Else
+    Day2: Conditional Statements: Switch
 
-    Complete the getGrade(score) function in the editor.
-    It has one parameter: an integer, score, denoting the number of points Julia earned on an exam.
-    It must return the letter corresponding to her grade according to the following rules:
+    Complete the getLetter(s) function in the editor.
+    It has one parameter: a string, s, consisting of lowercase English alphabetic letters (i.e., a through z).
+    It must return A, B, C, or D depending on the following criteria:
 
-    ・If 25 < score <= 30, then grade = A.
+    ・If the first character in string s is in the set {a, e, i, o, u}, then return A.
 
-    ・If 20 < score <= 25, then grade = B.
+    ・If the first character in string s is in the set {b, c, d, f, g}, then return B.
 
-    ・If 15 < score <= 20, then grade = C.
+    ・If the first character in string s is in the set {h, j, k, l, m}, then return C.
 
-    ・If 10 < score <= 15, then grade = D.
-
-    ・If 5 < score <= 10, then grade = E.
-
-    ・If 0 <= score <= 5, then grade = F.
+    ・If the first character in string s is in the set {n, p, q, r, s, t, v, w, x, y, z}, then return D.
 */
 
 'use strict';
@@ -25,22 +21,34 @@ function getLetter(s) {
     // Write your code here
     let firstChar = s.charAt(0);
     switch (firstChar) {
-        case "a" || "e" || "i" || "o" || "u" :
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
             letter = "A";
             break;
 
-        case "b" || "c" || "d" || "f" || "g" :
+        case "b":
+        case "c":
+        case "d":
+        case "f":
+        case "g":
             letter = "B";
             break;
 
-        case "h" || "j" || "k" || "l" || "m" :
+        case "h":
+        case "j":
+        case "k":
+        case "l":
+        case "m":
             letter = "C";
             break;
 
         default:
-            letter = "D";
+            letter = "D"
             break;
     }
     console.log(letter);
 }
-getLetter("dwxya");
+getLetter("adfgt");
